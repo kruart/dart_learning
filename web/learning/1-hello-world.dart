00b60be354cc67916608e183dde163f790448415
we0z <weoz@ukr.net>
2019-01-08 11:51:54 +0200

    hello-world

---
+++ web/learning/1-hello-world.dart
@@ -0,0 +1,11 @@
+// Define a function.
+printInteger(int aNumber) {
+  print('The number is $aNumber.'); // Print to console.
+}
+
+// This is where the app starts executing.
+main() {
+  print('Hello World');
+  var number = 42; // Declare and initialize a variable.
+  printInteger(number); // Call a function.
+}
